448ebd7dd06b187d9441d067f3027bb058016834
Francesco Meloni - francesco.meloni6@studio.unibo.it <francesco.meloni6@studio.unibo.it>
2024-11-19 16:16:57 +0100

    ex92 controller done

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,44 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    
+    private final static String OUTPUT_FILE = System.getProperty("user.home") +
+        System.getProperty("file.separator") + "output.txt";
 
+    private File file;
+
+    public Controller() {
+        file = new File(OUTPUT_FILE);
+    }
+
+    public Controller(final File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getPath() {
+        return this.file.getAbsolutePath();
+    }
+
+    public void readAndSave(final String string) throws IOException{
+        Objects.requireNonNull(string);
+        PrintStream ps = new PrintStream(file);
+        ps.println(string);
+        ps.close();
+    }
 }
