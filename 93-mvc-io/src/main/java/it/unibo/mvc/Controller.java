e8a26980b639e4898f59c0f6fe5ca9b04c064657
Francesco Meloni <francesco.meloni6@studio.unibo.it>
2024-11-19 20:13:33 +0100

    ex93 implemented the interface Controller.java

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,31 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * Set the next string to print.
+     * @param nextString 
+     */
+    public void setNextString(String nextString);
+
+    /**
+     * @return the next string to print.
+     */
+    public String getNextString();
+
+    /**
+     * @return the history of the printed strings.
+     */
+    public List<String> getHistory();
+
+    /**
+     * Print the current string.
+     * @exception IllegalStateException if the current string is unset
+     */
+    public void printCurrentString();
 }
